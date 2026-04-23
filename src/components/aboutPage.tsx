1bbd94cf138f205ef02833c324e249eaba189a22
Tharun Karthick N <tharunkarthick9384@gmail.com>
2026-04-23 18:59:05 +0530

    initial commit

---
+++ src/components/aboutPage.tsx
@@ -0,0 +1,6 @@
+const AboutPage = () => {
+
+}
+
+export default AboutPage;
+
---
+++ src/components/homePage.tsx
@@ -0,0 +1,11 @@
+const Homepage =() =>{
+    return(
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
